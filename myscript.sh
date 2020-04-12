b8b6de1df580d2ffd833d8842500599a23b79697
Shou Tseng <yakushou730@gmail.com>
2020-04-12 17:41:45 +0800

    math logic

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
 
-read -p "What's yout name " yourname
-echo "hello, $yourname. Nice to meet you" 
+number1=10
+echo "$(( number1 += 10 ))"
+
+if [ $number1 -eq 20 ]; then
+  echo "this is true"
+fi
+
+if [ $number1 -eq 30 ]; then
+  echo "this is false"
+fi
+
 
 # this is a comment
